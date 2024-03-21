b420aa6f5244bc753b6f93c3b21d1b2c9d149131
znichola <niki.zivkovic@hotmail.com>
2024-03-21 11:42:52 +0100

    bit multiplication

--- ex00/adder.hs
+++ ex00/adder.hs
@@ -1,42 +1,35 @@
 module Adder where
 
 import Data.Bits
-import Data.Foldable
-import Data.Int
 import Data.Word
 
--- printBits :: Int8 -> IO ()
--- printBits x = putStrLn (showFiniteBits x)
+-- haskell Word32 is equal to c unsigned long
 
-adder :: Int32 -> Int32 -> Int32
+adder :: Word32 -> Word32 -> Word32
 adder = go
   where
     go a 0 = a
     go a b = go (xor a b) (shiftL carry 1) where carry = a .&. b
 
--- until' p f = go
---   where
---     go x
---       | p x = x
---       | otherwise = go (f x)
+-- reference impletmentation
 
 -- def Bitwise_add(a,b):
 --     while b != 0:
---         carry = a&b # Carry value is calculated
---         a = a^b # Sum value is calculated and stored in a
---         b = carry<<1 # The carry value is shifted towards left by a bit
---     return a # returns the final sum
+--         carry = a&b
+--         a = a^b
+--         b = carry<<1
+--     return a
 
 -- Utility functions for printing binary reprisenttion
 
-getBitsArray :: Int32 -> [Bool]
+getBitsArray :: Word32 -> [Bool]
 -- implemented with a lambda expression for the function
 getBitsArray x = fmap (\pos -> 0 /= x .&. shiftL 1 pos) [31, 30 .. 0]
 
 -- implemented using chain opetator
 -- getBitsArray x = fmap ((0 /=) . (x .&.) . shiftL 1) [31, 30 .. 0]
 
-getBits :: Int32 -> String
+getBits :: Word32 -> String
 getBits x = fmap (showBitAt x) [31, 30 .. 0]
   where
     showBitAt x pos = if (x .&. shiftL 1 pos) /= 0 then '1' else '0'
---
+++ ex01/multiplier.hs
@@ -0,0 +1,36 @@
+module Multiplier where
+
+import Data.Bits
+import Data.Word
+
+multiplier :: Word32 -> Word32 -> Word32
+multiplier = go 0
+  where
+    go res _ 0 = res
+    go res 0 _ = res
+    go res a b = go (if b .&. 1 == 1 then adder res a else res) (shiftL a 1) (shiftR b 1)
+
+adder :: Word32 -> Word32 -> Word32
+adder = go
+  where
+    go a 0 = a
+    go a b = go (xor a b) (shiftL carry 1) where carry = a .&. b
+
+-- reference impletmentation
+
+-- unsigned int mult(x, y)
+-- unsigned int x, y;
+-- {
+--     unsigned int reg = 0;
+
+--     while (y != 0)
+--     {
+--         if (y & 1)
+--         {
+--             reg += x;
+--         }
+--         x <<= 1;
+--         y >>= 1;
+--     }
+--     return reg;
+-- }
